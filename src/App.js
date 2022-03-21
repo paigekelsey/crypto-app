43d68f8e8bf85e58f809084cc6aca9b9f2d95506
Kelsey Smith <pages.kelsey@gmail.com>
2022-03-21 15:20:16 -0400

    added custom logo design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
+// eslint-disable-next-line no-unused-vars
 import { Switch, Route, Link } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
 import { Layout, Typography, Space } from "antd"
+// eslint-disable-next-line no-unused-vars
+import {Navbar} from './components'
+import './App.css'
 
 const App = () => {
   return (
 <div className = 'app'>
     <div className = "navbar">
-
+<Navbar/>
     </div>
     <div className = 'main'>
 
     </div>
     <div className="footer">
-        
+
     </div>
 </div>
   )
