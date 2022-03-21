fac31578f0e9b084e4b70b8d4c420efc9cedcebe
Kelsey Smith <pages.kelsey@gmail.com>
2022-03-21 15:58:17 -0400

    implemented jsx components flow

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+    <Router>
+   
+        <App />
+   
+    </Router>
+,  document.getElementById('root'),
 );
