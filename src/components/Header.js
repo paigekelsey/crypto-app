fc8f8f39c2426dee66baea67896608ad2a6e9c86
Kelsey Smith <pages.kelsey@gmail.com>
2022-03-22 13:09:15 -0400

    added toolbar functionality - responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,69 +5,94 @@ import {
   Select,
   Toolbar,
   Typography,
+  IconButton,
+  Tooltip
 } from "@material-ui/core";
 import {
   makeStyles,
 } from "@material-ui/core/styles";
+import InfoIcon from '@mui/icons-material/Info';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 import { useHistory } from "react-router-dom";
 import { CryptoState } from "../CryptoContext";
+import Box from '@mui/material/Box';
 
 
 const useStyles = makeStyles(() => ({
   title: {
-    flex: 1,
-    color: "#5C6BC0",
+ textAlign:"center",
+ color: "#5C6BC0",
+ '&:hover': {
+    color: "#E05A47",
+    value: "icon"
+},
     fontFamily: "Montserrat",
     fontWeight: "bold",
     cursor: "pointer",
   },
-}));
+  menuIcon: {
+    flex:"1",
+    textAlign:'right',
+    justifyContent:"space-between",
+    cursor:"pointer",
+    alignItems:"center"
+  },
+  icons: {
+    margin:"1rem",
+    color: "#5C6BC0",
+    '&:hover': {
+       color: "#E05A47",
+  }
+}}));
 
 
 
 function Header() {
   const classes = useStyles();
   const { currency, setCurrency } = CryptoState();
-
   const history = useHistory();
 
   return (
 
       <AppBar color="transparent" position="static">
-        <Container style={{padding:"2rem"}}>
+        <Container style={{padding:"2rem", width:"100%"}}>
           <Toolbar>
-            <Typography
-              onClick={() => history.push(`/about`)}
-              variant="h6"
-              className={classes.title}
-              style={{ fontSize: 18 }}
-
-            >
-              About
-            </Typography>
-            <Typography
-              onClick={() => history.push(`/glossary`)}
-              variant="h6"
-              className={classes.title}
-              style={{ fontSize: 18 }}
-
-            >
-              Glossary
-            </Typography>
-            <img
-             src='./2.png'
-             width='100px' height='100px'
-             alt="crypto"/>
+          
             <Typography
               onClick={() => history.push(`/`)}
               variant="h6"
               className={classes.title}
             >
+                   <img
+             src='./2.png'
+             className={classes.title}
+             width='100px' height='100px'
+             alt="crypto"/>
               cryptoCipher
             </Typography>
-           
-            {/* <Button color="inherit">Login</Button>*/}
-
+            <div className={classes.menuIcon}>
+              <div id="dict-text">
+              <p className="text-dict">Glossary</p>
+            <MenuBookIcon
+              onClick={() => history.push(`/glossary`)}
+              variant="h6"
+              fontSize="medium"
+              className={classes.icons}
+              id="hover"
+           />
+           </div>
+           <div id="about-text">
+             <p className="text-about">About</p>
+            <InfoIcon
+              onClick={() => history.push(`/about`)}
+              variant="h6"
+              fontSize="medium"
+              className={classes.icons}
+              id="hover"
+            />
+            </div>
+             </div>
+            
             <Select
               variant="outlined"
               labelId="demo-simple-select-label"
@@ -75,12 +100,14 @@ function Header() {
               value={currency}
               style={{ width: 100, height: 40, marginLeft: 15 }}
               onChange={(e) => setCurrency(e.target.value)}
+             
             >
               <MenuItem value={"USD"}>USD</MenuItem>
               <MenuItem value={"INR"}>INR</MenuItem>
               <MenuItem value={"EUR"}>EUR</MenuItem>
               <MenuItem value={"JPY"}>JPY</MenuItem>
             </Select>
+        
           </Toolbar> 
         </Container>
       </AppBar>
