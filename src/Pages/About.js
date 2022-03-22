cdaa4428d7ed6fb881e0139e21a0149f09b2d2ee
Kelsey Smith <pages.kelsey@gmail.com>
2022-03-22 13:57:29 -0400

    added cards to about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,55 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
+import {GitHubIcon} from '@mui/icons-material';
+import { Paper } from '@mui/material';
+import { Card, CardContent } from '@material-ui/core';
 
+
+  
+
+  
 const PageM = () => {
+
   return (
+      <div className="card-wrapper">
+      <div className ="card-container1">
+    <Card  >
+        <CardContent>
     <div className="about-container">
-      <h1>created by Kelsey Smith</h1>
+       
+      <h5>Created by:</h5>
+       <h4>Kelsey Smith</h4>
       <GitHubIcon/>
+   
+    </div>
+    </CardContent>
+    </Card>
+    </div>
+    <div className ="card-container2">
+    <Card  >
+        <CardContent>
+    <div className="about-container">
+       
+      <h5>Created by:</h5>
+       <h4>Kelsey Smith</h4>
+      <GitHubIcon/>
+   
+    </div>
+    </CardContent>
+    </Card>
+    </div>
+    <div className ="card-container3">
+    <Card  >
+        <CardContent>
+    <div className="about-container">
+       
+      <h5>Created by:</h5>
+       <h4>Kelsey Smith</h4>
+      <GitHubIcon/>
+   
+    </div>
+    </CardContent>
+    </Card>
+    </div>
     </div>
   );
 };
